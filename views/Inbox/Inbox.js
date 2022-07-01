3b8435edd9c8544174945af302445de37df206d5
sunil <sunil@dzangolab.com>
2022-07-01 21:21:50 +0545

    fix: updated css

--- views/Inbox/Inbox.js
+++ views/Inbox/Inbox.js
@@ -1,36 +1,186 @@
 import React, {useState} from 'react';
-import {View} from 'react-native';
+import {ScrollView, StyleSheet, TextInput, View} from 'react-native';
 import Message from './Message';
 
 const Inbox = ({navigation, route}) => {
-  const [messages, setMessages] = useState([]);
-
-  console.log(route.params.accessToken);
-
-  fetch('https://api.pushbullet.com/v2/pushes', {
-    method: 'GET',
-    headers: {
-      'Access-Token': route.params.accessToken,
+  const [messages, setMessages] = useState([
+    {
+      type: 'note',
+      name: 'no name 1',
+      body: 'This is the body of the message. Hello 1',
     },
-  })
-    .then(response => response.json())
-    .then(json => {
-      console.log(json.pushes);
-      setMessages(json.pushes);
+    {
+      type: 'note',
+      name: 'no name 2',
+      body: 'This is the body of the message. Hello 2',
+    },
+    {
+      type: 'note',
+      name: 'no name 3',
+      body: 'This is the body of the message. Hello 3',
+    },
+    {
+      type: 'note',
+      name: 'no name 4',
+      body: 'This is the body of the message. Hello 4',
+    },
+    {
+      type: 'note',
+      name: 'no name 1',
+      body: 'This is the body of the message. Hello 1',
+    },
+    {
+      type: 'note',
+      name: 'no name 2',
+      body: 'This is the body of the message. Hello 2',
+    },
+    {
+      type: 'note',
+      name: 'no name 3',
+      body: 'This is the body of the message. Hello 3',
+    },
+    {
+      type: 'note',
+      name: 'no name 4',
+      body: 'This is the body of the message. Hello 4',
+    },
+    {
+      type: 'note',
+      name: 'no name 1',
+      body: 'This is the body of the message. Hello 1',
+    },
+    {
+      type: 'note',
+      name: 'no name 2',
+      body: 'This is the body of the message. Hello 2',
+    },
+    {
+      type: 'note',
+      name: 'no name 3',
+      body: 'This is the body of the message. Hello 3',
+    },
+    {
+      type: 'note',
+      name: 'no name 4',
+      body: 'This is the body of the message. Hello 4',
+    },
+    {
+      type: 'note',
+      name: 'no name 1',
+      body: 'This is the body of the message. Hello 1',
+    },
+    {
+      type: 'note',
+      name: 'no name 2',
+      body: 'This is the body of the message. Hello 2',
+    },
+    {
+      type: 'note',
+      name: 'no name 3',
+      body: 'This is the body of the message. Hello 3',
+    },
+    {
+      type: 'note',
+      name: 'no name 4',
+      body: 'This is the body of the message. Hello 4',
+    },
+    {
+      type: 'note',
+      name: 'no name 1',
+      body: 'This is the body of the message. Hello 1',
+    },
+    {
+      type: 'note',
+      name: 'no name 2',
+      body: 'This is the body of the message. Hello 2',
+    },
+    {
+      type: 'note',
+      name: 'no name 3',
+      body: 'This is the body of the message. Hello 3',
+    },
+    {
+      type: 'note',
+      name: 'no name 4',
+      body: 'This is the body of the message. Hello 4',
+    },
+    {
+      type: 'note',
+      name: 'no name 1',
+      body: 'This is the body of the message. Hello 1',
+    },
+    {
+      type: 'note',
+      name: 'no name 2',
+      body: 'This is the body of the message. Hello 2',
+    },
+    {
+      type: 'note',
+      name: 'no name 3',
+      body: 'This is the body of the message. Hello 3',
+    },
+    {
+      type: 'note',
+      name: 'no name 4',
+      body: 'This is the body of the message. Hello 4',
+    },
+  ]);
+
+  const fetchMessages = () => {
+    fetch('https://api.pushbullet.com/v2/pushes', {
+      method: 'GET',
+      headers: {
+        'Access-Token': route.params.accessToken,
+      },
     })
-    .catch(error => {
-      console.log(error);
-    });
+      .then(response => response.json())
+      .then(json => {
+        setMessages(json.pushes);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   return (
-    <View>
-      {messages && messages.length
-        ? messages.map(message => {
-            return <Message message={message} key={message.index} />;
-          })
-        : null}
+    <View style={styles.inboxView}>
+      <ScrollView contentContainerStyle={styles.messagesList}>
+        {messages && messages.length
+          ? messages.map(message => {
+              return (
+                <Message
+                  message={message}
+                  key={message.name}
+                  style={styles.messageBox}
+                />
+              );
+            })
+          : null}
+      </ScrollView>
+      <TextInput style={styles.messageInput} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  inboxView: {
+    flex: 1,
+  },
+  messageInput: {
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 1,
+    paddingLeft: 10,
+  },
+  messagesList: {
+    alignItems: 'flex-end',
+    borderColor: 'blue',
+    borderWidth: 2,
+    flex: 1,
+    justifyContent: 'flex-end',
+    padding: 10,
+    textAlign: 'right',
+  },
+});
 
 export default Inbox;
--- views/Inbox/Message.js
+++ views/Inbox/Message.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 
 const Message = props => {
   const message = props.message;
 
   return (
-    <Text key={message.name}>
+    <Text key={message.name} style={styles.messageBox}>
       {message.type === 'note' ? message.body : message.file_name}
     </Text>
   );
 };
+
+const styles = StyleSheet.create({
+  messageBox: {
+    borderColor: 'blue',
+    borderWidth: 1,
+    marginTop: 10,
+    padding: 5,
+    textAlign: 'right',
+    width: '50%',
+  },
+});
 
 export default Message;
